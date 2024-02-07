d12845044fcd640c050f6f316a53be0b81160ad2
yolanda1926 <159155387+yolanda1926@users.noreply.github.com>
2024-02-07 12:24:20 -0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,19 @@
+let numeroSecreto = 5; 
+
+var chute = prompt("digite um numero de 1 a 10"); 
+
+while( chute != numeroSecreto){
+
+if(numeroSecreto == chute){
+    alert("voce acertou") 
+}else{
+    alert("voce errou")
+    if(chute < numeroSecreto){
+        alert("o numero secreto é menor que ", chute)
+    }else{
+        alert("o numero secreto é menor que ", chute)
+    }
+
+}
+var chute = prompt("digite um numero de 1 a 10")
+}
